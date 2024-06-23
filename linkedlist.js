1e7303c34492a2ae454392a408907918d599c1c3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-23 17:36:13 +0200

    Add append function
    The function append a node to the end of the list

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,17 @@
 export function LinkedList() {
   let head = null;
+
+  const append = (node) => {
+    if (!head) {
+      head = node;
+    } else {
+      let previous = head;
+      while (previous.next !== null) {
+        previous = previous.next;
+      }
+      previous.next = node;
+    }
+  };
+
+  return { head, append };
 }
